c5ce766a05e9185479d42fb6caa4b641368d6f7b
Sofonias Ayele <sof16ayele@gmail.com>
2022-10-25 00:46:21 +0000

    Task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 The Flask web app
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,6 +16,12 @@ app.register_blueprint(app_views)
 def teardown_session(exception):
     """ Closes storage session """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Returns JSON response with 404 status """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
