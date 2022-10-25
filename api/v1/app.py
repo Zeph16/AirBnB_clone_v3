77ada65f34d33ac47528ed52a7113e03d132f42e
Sofonias Ayele <sof16ayele@gmail.com>
2022-10-25 01:51:36 +0000

    Task 14

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,7 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.teardown_appcontext
 def teardown_session(exception):
