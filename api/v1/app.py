42ab7592e4adc28e5674e473c817d80f1beabe88
Sofonias Ayele <sof16ayele@gmail.com>
2022-10-25 01:27:58 +0000

    Task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,14 +3,14 @@
 The Flask web app
 """
 from flask import Flask, make_response, jsonify
+from flask import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+CORS(app, resources={r"/*":{"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_session(exception):
