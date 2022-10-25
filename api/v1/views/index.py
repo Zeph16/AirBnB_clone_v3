8d2293c46ee0108d6e0ae1482f2401079dc13f3a
Sofonias Ayele <sof16ayele@gmail.com>
2022-10-25 00:55:06 +0000

    Task 6

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,13 +5,13 @@ from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns JSON """
     return jsonify(status="OK")
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """ Returns the number of each instance type """
     return jsonify(amenities=storage.count("Amenity"),
